c1bb825a58883521051345f8ca96194ce6ea9730
Hrithik Raj <hrithikraj6400@gmail.com>
2025-02-06 23:51:41 +0530

    color flipper

--- src/new.js
+++ src/new.js
@@ -1,11 +1,24 @@
-let saveEl = null
-let countEl = null
 let count = 0
+let countEl = null
+let saveEl = null
 
 window.addEventListener("DOMContentLoaded", (e) => {
     saveEl = document.getElementById('save-el')
     countEl = document.getElementById('count-el')
+    colors = ["green", "red", "rgba(133,122,200)", "#f15025"]
+    btn = document.getElementById("btn")
+    color = document.querySelector(".color")
+    btn.addEventListener("click", function () {
+        randomNumber = getRandomNumber()
+        console.log(randomNumber)
+        document.body.style.backgroundColor = colors[randomNumber]
+        color.textContent = colors[randomNumber]
+    })
 })
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length)
+}
 
 function increment() {
     count += 1
@@ -21,3 +34,6 @@ function save() {
     count = 0
     countEl.textContent = 0
 }
+
+
+
