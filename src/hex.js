c613ca4dd0f41759569cdfd777049ab57f18ca40
Hrithik Raj <hrithikraj6400@gmail.com>
2025-02-27 00:47:49 +0530

    navbar

--- src/hex.js
+++ src/hex.js
@@ -80,7 +80,15 @@ window.addEventListener("DOMContentLoaded", (e) => {
       currentitem = Math.floor(Math.random() * reviews.length)
       showPerson(currentitem)
     })
-    
+
+    btn = document.getElementById('menu-btn')
+    nav = document.getElementById('menu')
+
+    btn.addEventListener('click', function () {
+      btn.classList.toggle('open')
+      nav.classList.toggle('flex')
+      nav.classList.toggle('hidden')
+    })
 })
 
 function getRandomNumber() {
