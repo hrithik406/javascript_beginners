d91d69ce5b505b11be4d6a1a14cb5843bedc608f
Hrithik Raj <hrithikraj6400@gmail.com>
2025-03-15 01:23:09 +0530

    sidebar

--- src/hex.js
+++ src/hex.js
@@ -1,98 +1,122 @@
 const reviews = [
-    {
-      id: 1,
-      name: 'susan smith',
-      job: 'web developer',
-      img: 'https://www.course-api.com/images/people/person-1.jpeg',
-      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    },
-    {
-      id: 2,
-      name: 'anna johnson',
-      job: 'web designer',
-      img: 'https://www.course-api.com/images/people/person-2.jpeg',
-      text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
-    },
-    {
-      id: 3,
-      name: 'peter jones',
-      job: 'intern',
-      img: 'https://www.course-api.com/images/people/person-4.jpeg',
-      text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
-    },
-    {
-      id: 4,
-      name: 'bill anderson',
-      job: 'the boss',
-      img: 'https://www.course-api.com/images/people/person-3.jpeg',
-      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
-    },
-  ]
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    img: 'https://www.course-api.com/images/people/person-1.jpeg',
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    img: 'https://www.course-api.com/images/people/person-2.jpeg',
+    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    img: 'https://www.course-api.com/images/people/person-4.jpeg',
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    img: 'https://www.course-api.com/images/people/person-3.jpeg',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+]
 
-  let currentitem = 0
+let currentitem = 0
 
 window.addEventListener("DOMContentLoaded", (e) => {
-    hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
-    btn = document.getElementById("btn")
-    color = document.querySelector(".color")
-    btn.addEventListener("click", function () {
-        let hexcolor = "#"
-        for(let i = 0; i < 6; i++){
-            hexcolor += hex[getRandomNumber()]
-        }
-        console.log(hexcolor)
-        document.body.style.backgroundColor = hexcolor
-        color.textContent = hexcolor
-    })
+  hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+  btn = document.getElementById("btn")
+  color = document.querySelector(".color")
+  btn.addEventListener("click", function () {
+    let hexcolor = "#"
+    for (let i = 0; i < 6; i++) {
+      hexcolor += hex[getRandomNumber()]
+    }
+    console.log(hexcolor)
+    document.body.style.backgroundColor = hexcolor
+    color.textContent = hexcolor
+  })
 
-    img = document.getElementById('img')
-    author = document.getElementById('author')
-    job = document.getElementById('job')
-    info = document.getElementById('info')
+  img = document.getElementById('img')
+  author = document.getElementById('author')
+  job = document.getElementById('job')
+  info = document.getElementById('info')
 
-    nextbtn = document.querySelector('#next-btn')
-    prevbtn = document.querySelector('#prev-btn')
-    randombtn = document.querySelector('#random-btn')
+  nextbtn = document.querySelector('#next-btn')
+  prevbtn = document.querySelector('#prev-btn')
+  randombtn = document.querySelector('#random-btn')
 
+  showPerson(currentitem)
+
+  nextbtn.addEventListener('click', function () {
+    console.log('next')
+    currentitem++
+    if (currentitem > reviews.length - 1) {
+      currentitem = 0
+    }
     showPerson(currentitem)
+  })
 
-    nextbtn.addEventListener('click', function () {
-      console.log('next')
-      currentitem++
-      if (currentitem > reviews.length - 1) {
-        currentitem = 0
-      }
-      showPerson(currentitem)
-    })
-    
-    prevbtn.addEventListener('click', function () {
-      console.log('previous')
-      currentitem--
-      if (currentitem < 0) {
-        currentitem = reviews.length - 1
-      }
-      showPerson(currentitem)
-    })
-    
-    randombtn.addEventListener('click', function () {
-      console.log('random')
-    
-      currentitem = Math.floor(Math.random() * reviews.length)
-      showPerson(currentitem)
-    })
+  prevbtn.addEventListener('click', function () {
+    console.log('previous')
+    currentitem--
+    if (currentitem < 0) {
+      currentitem = reviews.length - 1
+    }
+    showPerson(currentitem)
+  })
 
-    btn = document.getElementById('menu-btn')
-    nav = document.getElementById('menu')
+  randombtn.addEventListener('click', function () {
+    console.log('random')
 
-    btn.addEventListener('click', function () {
-      btn.classList.toggle('open')
-      nav.classList.toggle('flex')
-      nav.classList.toggle('hidden')
-    })
+    currentitem = Math.floor(Math.random() * reviews.length)
+    showPerson(currentitem)
+  })
+
+  btn = document.getElementById('menu-btn')
+  nav = document.getElementById('menu')
+
+  btn.addEventListener('click', function () {
+    btn.classList.toggle('open')
+    nav.classList.toggle('flex')
+    nav.classList.toggle('hidden')
+  })
+
+  const togglebtn = document.querySelector("#sidebar-toggle")
+  const closebtn = document.querySelector("#close-btn")
+  const sidebar = document.querySelector(".sidebar")
+  const sidebarBg = document.querySelector("#sidebar-bg")
+
+  function toogleSiderBar() {
+    sidebar.classList.toggle("show-sidebar")
+    sidebarBg.classList.toggle("opacity-0")
+    sidebarBg.classList.toggle("pointer-events-none")
+  }
+
+  togglebtn.addEventListener("click", function () {
+    toogleSiderBar()
+  })
+
+  closebtn.addEventListener("click", function () {
+    toogleSiderBar()
+  })
+
+  sidebarBg.addEventListener("click", function () {
+    toogleSiderBar()
+  })
+
 })
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * hex.length)
+  return Math.floor(Math.random() * hex.length)
 }
 
 function showPerson(person) {
