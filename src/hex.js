a98d9c93f81b9e0b052c63a7e3bd89bb063f59fa
Hrithik Raj <hrithikraj6400@gmail.com>
2025-03-22 00:28:20 +0530

    question updated

--- src/hex.js
+++ src/hex.js
@@ -130,6 +130,22 @@ window.addEventListener("DOMContentLoaded", (e) => {
     togglemodal()
   })
 
+  const ques = document.querySelectorAll("#question")
+
+  ques.forEach(function (question){
+    const quesbtn = question.querySelector("question-btn")
+
+    quesbtn.addEventListener("click", function() {
+      ques.forEach(function (item) {
+        if (item !== question){
+          item.classList.remove("show-text")
+        }
+      })
+
+      question.classList.toggle("show-text")
+    })
+  })
+
 })
 
 function getRandomNumber() {
