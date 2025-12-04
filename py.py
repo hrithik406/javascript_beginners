a05ce2925fe782881525fe8c4f847654474495c9
Hrithik Raj <hrithikraj6400@gmail.com>
2025-12-04 14:28:03 +0530

    game trials

---
+++ py.py
@@ -0,0 +1,64 @@
+# stri = str(input("enter the string: "))
+# a = stri.lower()
+# vowels = "aeiou"
+# # print(stri)
+# # print(len(stri))
+# # print(stri[len(stri)-1::-1])
+# # count = 0
+# # for i in a:
+# #     if i in vowels:
+# #         count+=1
+
+# # print(count)
+
+# v = " ".join(ch for ch in a if ch in vowels)
+# print(v)
+# print(vowels[:3])
+# print(vowels[len(vowels)-3:])
+
+
+# list1 = []
+
+# n = int(input("enter the range of list: "))
+
+# for i in range(n):
+#     a = int(input(f"enter the item number{i}: "))
+#     list1.append(a)
+
+# print(list1)
+# def evn_squares():
+#     list2=[]
+#     for j in list1:
+#         if j % 2 == 0:
+#             list2.append(j)
+
+#     print(list2)
+
+#     list3 = []
+#     for k in list1:
+#         if k % 2 == 0:
+#             list3.append(k**2)
+
+#     return list3
+
+# print(evn_squares()) 
+
+# list1 = []
+# for i in range(10):
+#     a = int(input(f"enter the numbers{i+1}: "))
+#     list1.append(a)
+
+# print(list1[:3])
+# print(list1[-3:])
+# print(max(list1))
+# print(min(list1))
+# print(sum(list1))
+
+a = input("enter the number: ")
+b = input("enter the string: ")
+c = int(a)
+d = str(b).upper()
+print(c)
+print(d)
+print(type(c))
+print(type(d))
